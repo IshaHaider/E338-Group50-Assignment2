1a3d83535f2998a9372d5c35728d120184968a22
ishahaider <isha.haider213@gmail.com>
2023-02-12 05:39:19 -0700

    Update ex2.2.py

--- ex2.2.py
+++ ex2.2.py
@@ -1,12 +1,16 @@
 
 import sys
+import random
 sys.setrecursionlimit(20000)
 def func1(arr, low, high):
     if low < high:
         pi = func2(arr, low, high)
         func1(arr, low, pi-1)
         func1(arr, pi + 1, high)
+
 def func2(array, start, end):
+    pivotIndex = random.randint(start, end) 
+    array[start], array[pivotIndex] = array[pivotIndex], array[start]
     p = array[start]
     low = start + 1
     high = end
@@ -22,7 +26,6 @@ def func2(array, start, end):
     array[start], array[high] = array[high], array[start]
     return high
 
-
 if __name__ == '__main__': 
 
     import json 
@@ -33,20 +36,26 @@ if __name__ == '__main__':
     with open("ex2.json", "r") as inFile: 
         content = json.load(inFile)
     
-    # Timing all executions
+    
     timearray = []
     arraylen = []
-    rez = []
+    #sorted_arrays = [] #REMOVE BEFORE SUBMISSION 
+    # iterate through each array and time how long it takes to sort it
     for array in content:
         arraylen.append(len(array))
         tm = timeit.timeit(lambda: func1(array, 0, len(array)-1),number=1)
         timearray.append(tm)
         print("Time for sorting array of length %d: %f seconds" % (len(array), tm))
-    
+        
+        #func1(array, 0, len(array)-1) #REMOVE BEFORE SUBMISSION 
+        #sorted_arrays.append(array) #REMOVE BEFORE SUBMISSION 
+        
+    #with open("ex2.7.json", "w") as outFile: #REMOVE BEFORE SUBMISSION 
+        #json.dump(sorted_arrays, outFile) #REMOVE BEFORE SUBMISSION 
     # plot timing results
     plt.scatter(arraylen, timearray)
     plt.xlabel("Length of Array (n)")
     plt.ylabel("Time of Quick Sort (sec)")
-    plt.title("Timing Results of Sorting Various Arrays")
+    plt.title("Timing Results of Sorting Arrays of Various Lengths")
     plt.show()
-
+    
